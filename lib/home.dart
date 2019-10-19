73ab7b550f3c7651671a6deb12bcfecfe9d85511
mohitmalviya20 <mohitmalviya20@gmail.com>
2019-10-20 00:44:52 +0530

    the vigilante

---
+++ lib/home.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:quizstar/quizpage.dart';
+
+class homepage extends StatefulWidget {
+  @override
+  _homepageState createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+
+  List<String> images = [
+    "images/earth.jpg",
+    "images/space.jpg",
+  ];
+
+  List<String> des = [
+    "Earth is the third planet from the Sun and the only astronomical object known to harbor life.Earth formed over 4.5 billion years ago.\n click to go to quiz",
+    "Space is the boundless three-dimensional extent in which objects and events have relative position and direction.Modern physicists consider time, to be part of a boundless 4D continuum known as spacetime. \n click to go to the quiz",
+  ];
+
+  Widget customcard(String langname, String image, String des){
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 20.0,
+        horizontal: 30.0,
+      ),
+      child: InkWell(
+        onTap: (){
+          Navigator.of(context).pushReplacement(MaterialPageRoute(
+            // in changelog 1 we will pass the langname name to ther other widget class
+            // this name will be used to open a particular JSON file 
+            // for a particular language
+            builder: (context) => getjson(langname),
+          ));
+        },
+        child: Material(
+          color: Colors.indigoAccent,
+          elevation: 10.0,
+          borderRadius: BorderRadius.circular(25.0),
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                  ),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(100.0),
+                    child: Container(
+                      // changing from 200 to 150 as to look better
+                      height: 150.0,
+                      width: 150.0,
+                      child: ClipOval(
+                        child: Image(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            image,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    langname,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontFamily: "Quando",
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    des,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white,
+                      fontFamily: "Alike"
+                    ),
+                    maxLines: 5,
+                    textAlign: TextAlign.justify,
+                  ),
+                  
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown, DeviceOrientation.portraitUp
+    ]);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Earth and Space",
+          style: TextStyle(
+            fontFamily: "Quando",
+          ),
+        ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          customcard("Earth quiz", images[0], des[0]),
+          customcard("space quiz", images[1], des[1]),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/splash.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:quizstar/home.dart';
+
+class splashscreen extends StatefulWidget {
+  @override
+  _splashscreenState createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+
+  @override
+  void initState(){
+    super.initState();
+    Timer(Duration(seconds: 3), (){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => homepage(),
+      ));
+    });
+  }
+
+  // added test yourself
+  // and made the text to align at center 
+  @override
+  Widget build(BuildContext context) {
+    var center = Center(
+        child:Text(
+          "Earth and Space\n\nA quiz that tests your geographical and astronomical skill!!!",
+          style: TextStyle(
+            fontSize: 50.0,
+            color: Colors.white,
+            fontFamily: "Satisfy",
+          ),
+          textAlign: TextAlign.center,
+        ),
+      );
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: center,
+    );
+  }
+}
